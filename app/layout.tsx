621b7bf5aab097769cd0153be4ba2ff30da3f005
Juan Carlos Alvarado <jc.alvarado.valdivia@gmail.com>
2023-12-21 23:21:45 -0300

    ci: adds vercel deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="flex justify-between gap-2 w-full">
+          <Link href="/">Home</Link>
+          <section>
+            <Link href="/category">Categorías</Link>
+            <Link href="/publish">Publicar</Link>
+          </section>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ app/product/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1>Hello, Product Page!</h1>
+}
